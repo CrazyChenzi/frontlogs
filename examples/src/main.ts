21b024336936cfc1d9ef3574d164593b1262fbec
林深处见鹿 <756319278@qq.com>
2020-05-19 15:25:22 +0800

    add 添加错误拦截 onerror 拦截 unhandledrejection 拦截 Vue.config.errorHandler 拦截

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.config.errorHandler = (err, vm, info) => {
+  console.log(`err: ${err}`, `vm:`, vm, `info: ${info}`)
+  return true
+}
 
 new Vue({
   render: h => h(App),
